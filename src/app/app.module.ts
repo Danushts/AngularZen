1e99efe1cedd819dcb0fe7f81e0dc5884b35f1c1
arjunamg <arjunamg1993@gmail.com>
2018-07-17 18:30:18 +0530

    Day5 code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
-import { AppComponent } from './app.component';
-import { ZenbannerComponent } from './zenbanner/zenbanner.component';
-import { ZenMenuComponent } from './zen-menu/zen-menu.component';
-import { ZeninformationComponent } from './zeninformation/zeninformation.component';
-import { ZenfooterComponent } from './zenfooter/zenfooter.component';
-import { TestdeleteComponent } from './testdelete/testdelete.component';
-import { ZenLoginComponent } from './zen-login/zen-login.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { ZenbannerComponent } from "./zenbanner/zenbanner.component";
+import { ZenmenuComponent } from "./zenmenu/zenmenu.component";
+import { ZeninformationComponent } from "./zeninformation/zeninformation.component";
+import { ZenfooterComponent } from "./zenfooter/zenfooter.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     ZenbannerComponent,
-    ZenMenuComponent,
+    ZenmenuComponent,
     ZeninformationComponent,
-    ZenfooterComponent,
-    TestdeleteComponent,
-    ZenLoginComponent
+    ZenfooterComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
