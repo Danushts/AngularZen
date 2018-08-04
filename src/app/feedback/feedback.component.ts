1bcdde380970d2341e39f3e62193a26c4911ed5e
arjunamg <arjunamg1993@gmail.com>
2018-08-04 11:21:17 +0530

    day12_aug3_arjun

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,27 +1,46 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2
+} from "@angular/core";
 
 @Component({
-  selector: 'app-feedback',
-  templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  selector: "app-feedback",
+  templateUrl: "./feedback.component.html",
+  styleUrls: ["./feedback.component.css"]
 })
 export class FeedbackComponent implements OnInit {
-  @ViewChild('boxnew') boxvar:ElementRef;
-  show=true;
-  constructor(private rendererOb:Renderer2) { }
+  @ViewChild("boxnew") boxvar: ElementRef;
+  public inputCreated = false;
+  hideTick = true;
+  // tickArray = [{ tick: true }, { tick: true }, { tick: true }];
+  tickObject = {
+    course: [{ tick: true }, { tick: true }, { tick: true }],
+    instructor: [{ tick: true }, { tick: true }, { tick: true }],
+    improvement: [{ tick: true }, { tick: true }, { tick: true }]
+  };
+  constructor(private rendererOb: Renderer2) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggeleTick(property, index) {
+    // this.hideTick=!this.hideTick;
+    this.tickObject[property][index].tick = !this.tickObject[property][index].tick;
   }
 
-  addInputBox(){
-    this.show=false;
+  addInputBox() {
     console.log(this.boxvar);
-    let inputBox = this.rendererOb.createElement('input');
-    this.rendererOb.appendChild(this.boxvar.nativeElement,inputBox);
+    if (!this.inputCreated) {
+      let inputBox = this.rendererOb.createElement("input");
+      this.rendererOb.setAttribute(inputBox, "placeholder", "Other");
+      this.rendererOb.appendChild(this.boxvar.nativeElement, inputBox);
+      this.inputCreated = true;
+    }
 
-    let checkBox = this.rendererOb.createElement('input');
-    this.rendererOb.setAttribute(checkBox,'type','checkbox')
-    this.rendererOb.appendChild(this.boxvar.nativeElement,checkBox);
+    // let checkBox = this.rendererOb.createElement('input');
+    // this.rendererOb.setAttribute(checkBox,'type','checkbox')
+    // this.rendererOb.appendChild(this.boxvar.nativeElement,checkBox);
   }
-
 }
