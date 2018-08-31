a342ca3f6562cf2b3fb647e1cc0cd40775ba0479
arjunamg <arjunamg1993@gmail.com>
2018-08-31 08:22:12 +0530

    day21_aug30_arjun

--- src/app/httpclient/httpclient.component.ts
+++ src/app/httpclient/httpclient.component.ts
@@ -1,4 +1,7 @@
+import { HttpclientService } from './../service/httpclient.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpRequest,HttpEvent,HttpEventType } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-httpclient',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./httpclient.component.css']
 })
 export class HttpclientComponent implements OnInit {
-
-  constructor() { }
+  data;
+  constructor( private httpServiceOb:HttpclientService) { }
 
   ngOnInit() {
+    console.log(this.httpServiceOb.getCourses());
+    this.httpServiceOb.getCourses().subscribe((event:HttpEvent<any>)=>{
+      switch(event.type){
+        case HttpEventType.Sent:console.log('Request Sent!');
+        break;
+        case HttpEventType.ResponseHeader:
+        console.log('Request Header received!');
+          break;
+        case HttpEventType.DownloadProgress:
+          console.log(event.loaded); 
+          const pvar = Math.round(event.loaded);
+          console.log(`in Progress! ${ pvar } bytes loaded`);
+          break;
+        case HttpEventType.Response:
+          console.log('Done!',event.body);
+          this.data= event.body;
+      }
+    })
   }
 
 }
--- src/app/service/httpclient.service.ts
+++ src/app/service/httpclient.service.ts
@@ -1,9 +1,20 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpRequest } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpclientService {
-
-  constructor() { }
+  url = "/coursedetails.php";
+  constructor( private httpOb:HttpClient) { }
+  getCourses():Observable<any>{
+    const req = new HttpRequest('GET',this.url,{
+      reportProgress:true
+    });
+    console.log('came i getCourses Function');
+    return this.httpOb.request(req);
+  }
 }
